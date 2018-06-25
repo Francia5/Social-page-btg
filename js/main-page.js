42de8961d9a4080c70e6bd42ab722b5a0614f677
francia5 <francia.va.or@gmail.com>
2018-06-25 11:25:52 -0500

    se agrega funcionalidad al slide bar y creación de cards

---
+++ js/main-page.js
@@ -0,0 +1,55 @@
+$(document).ready(function () {
+    /*******BARRA LATERIAL DE USUARIO*******/
+    $('.sidenav').sidenav();
+
+    /******MENU DE INTERESES DE USUARIO********/
+    $('.collapsible').collapsible();
+
+    /***MANEJANDO DOM PARA CREAR CARDS******/
+    $('#btn-send').click(createCards);
+
+    var count = 0;
+
+    function createCards() {
+        var comment = $("#comment").val();
+
+        /*clean input */
+        var commentUser = $('#comment').val();
+
+        var template =
+            '<div id="card-user" class="row">' +
+            '<div class="col s10 offset-s1">' +
+            '<div class="card row grey lighten-4">' +
+            '<div class="col s12 card-content">' +
+            '<p class="col s10 offset-s1 express-yourself" >_prueba_</p>' +
+            '<div class="row">' +
+            '<div class="col s8 offset-s3 card-action">' +
+            '<a id="heart" href="#"><i class="tiny material-icons iconos-card icon-heart grey-text right">favorite</i></a>' +
+            '</div>' +
+            '</div>' +
+            '</div>' +
+            '</div>' +
+            '</div>';
+
+        /* limpiar template **/
+        var finalTemplate = "";
+
+        /* Remplazar la plantilla: el valor de cada comentario***/
+        finalTemplate = template.replace('_prueba_', commentUser);
+
+        /**Se le asigna donde se coloca la plantilla nueva**/
+        $('main').append(finalTemplate);
+
+        /**Limpiar el comentario por cada CARD creado**/
+        $('#comment').val('');
+
+        /*******TOOGLE PARA DAR "ME GUSTA"*********/
+        $('#heart').click(like);
+
+        function like() {
+            console.log('holi');
+        }
+
+    }
+
+});
