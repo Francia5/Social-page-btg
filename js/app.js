7e4564de77875c2f0103daf54e6d6d14b7a32480
francia5 <francia.va.or@gmail.com>
2018-06-25 11:14:40 -0500

    se agrega funcionalidad para login y app.js

---
+++ js/app.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+
+    // ******************FUNCION PARA SPLASH******************//
+    setTimeout(function () {
+        $('#splash').fadeOut(500);
+    }, 1000);
+
+    //*********INICIALIZACION CONTENEDOR LOG IN-SING UP*********//
+    $('.collapsible').collapsible();
+
+
+    $('#btn-next1').click(goToMainPage);
+
+    // *************FUNCION PARA ENLAZAR EL BOTON*************//
+    function goToMainPage(event) {
+        event.preventDefault();
+        window.location.href = 'view-principal.html';
+    }
+
+
+    // Modal subir Imagen
+    $('.modal').modal();
+
+
+
+
+});
+
---
+++ js/firebase.js
@@ -0,0 +1,99 @@
+var provider = new firebase.auth.GoogleAuthProvider();
+var fichero;
+// var storageRef = firebase.storage().ref();
+
+
+// Obtenemos la Data del Usuario por medio del provedor (google)
+$("#login").click(function () {
+    firebase.auth()
+        .signInWithPopup(provider)
+        .then(function (data) {
+            var user = data.user;
+            console.log(user);
+            $(".inicio").hide();
+            $(".usuario").show();
+            $(".user-image").attr("src", user.photoURL);
+            $("#name").text("Nombre: " + user.displayName);
+            $("#email").text(user.email);
+
+
+        }).catch(function (error) {
+            console.log(error);
+
+        });
+});
+
+// Mostramos o no la información del usuario de acuerdo a su status
+firebase.auth().onAuthStateChanged(function (user) {
+    if (user) {
+        // Si el usuario esta logeado
+        $(".inicio").hide();
+        $(".usuario").show();
+        $(".user-image").attr("src", user.photoURL);
+        $("#name").text("Nombre: " + user.displayName);
+        $("#email").text(user.email);
+    } else {
+        // Si no esta logeado
+        $(".inicio").show();
+        $(".usuario").hide();
+    }
+});
+
+
+$("#close").click(function () {
+    firebase.auth().signOut().then(function () {
+        $(".inicio").show();
+        $(".usuario").hide();
+    })
+});
+
+
+
+
+// function ju () {
+
+
+
+//     // console.log(storageRef);
+
+
+// };
+
+
+
+
+
+// function subirImagenAFirebase() {
+//     var photoUserAdd = fichero.files[0];
+//     // storageRef = firebase.storage().ref();
+
+//     // var uploadTask = storageRef.child("images/" + photoUserAdd.name).put(photoUserAdd);
+
+
+
+// };
+
+
+// Obtener Imagen para publicar
+
+var tablaBaseDatos = firebase.database().ref("imagenes");
+$("#card-image").change(function () {
+    if (this.files && this.files[0]) {
+        console.log(this.files[0].name);
+
+        var newFile = new FileReader();
+        // // console.log(newFile, newFile.result);
+        newFile.onload = function (e) {
+            tablaBaseDatos.push({
+                url: e.target.result
+            });
+
+            $("#fotousuario").attr("src", e.target.result);
+
+
+
+        }
+        newFile.readAsDataURL(this.files[0]);
+    }
+});
+
